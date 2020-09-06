945e40dd6e3c2a806eabb4489c6ba594dfbc38e5
Evan Seitz <35609503+evanseitz@users.noreply.github.com>
2020-09-05 23:20:49 -0400

    Add files via upload

--- 1_Embedding/DM/DiffusionMaps.py
+++ 1_Embedding/DM/DiffusionMaps.py
@@ -11,6 +11,7 @@ from pylab import imshow, show, loadtxt, axes
 import scipy
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import MaxNLocator
+import GaussianBandwidth
 
 # =============================================================================
 # Embed distance files for each PD via diffusion maps framework
@@ -34,8 +35,10 @@ from matplotlib.ticker import MaxNLocator
 def op(pyDir, PD):
     dataDir = os.path.join(pyDir, 'Data_Distances')
     outDir = os.path.join(pyDir, 'Data_Manifolds')
+    if not os.path.exists(outDir):
+        os.mkdir(outDir)
     Dist = np.load(os.path.join(dataDir, 'PD%s_tau5_SNR_dist.npy' % PD))
-    
+
     # =========================================================================
     # Distances matrix analysis
     # =========================================================================
@@ -59,25 +62,35 @@ def op(pyDir, PD):
         plt.show()
     
     # =========================================================================
-    # Method to estimate optimal epsilon; Ferguson (2010)
+    # Method to estimate optimal epsilon; see Ferguson SI (2010)
     # =========================================================================
-    if 0:
-        logEps0 = np.arange(-50,50.1,0.1)
-        logSumA0 = np.zeros(len(logEps0))
-        for k in xrange(len(logEps0)):
-            eps0 = np.exp(logEps0[k]) #temporary kernel bandwidth
-            A0 = np.exp(-1.*((Dist**2.) / (2.*eps0))) #temporary similarity matrix  
-            logSumA0[k] = np.log(np.sum(A0))
-        if 1: #plot of Ferguson method
-            plt.scatter(logEps0, logSumA0, s=1, c='C0', edgecolor='C0', zorder=.1)
-            plt.xlabel(r'$\mathrm{ln \ \epsilon}$', fontsize=16)
-            plt.ylabel(r'$\mathrm{ln \ \sum_{i,j} \ A_{i,j}}$', fontsize=18, rotation=90)
-            plt.show()
-        if 0: #save Ferguson plot to file
-            np.save('Ferg_SS1_PD%s.npy' % PD, [logEps0, logSumA0])
+    logEps = np.arange(-20,20.2,0.2)
+    a0 = 1*(np.random.rand(4,1)-.5)
+    popt, logSumWij, resnorm, R2 = GaussianBandwidth.op(Dist,logEps,a0)
     
-    eps = 1e4 #in optimal range for PDs supplied (SNR=0.1, tau=5); when using alternative...
-               #...datasets, alter based on above plot
+    def fun(xx, aa0, aa1, aa2, aa3): #fit tanh()
+        F = aa3 + aa2 * np.tanh(aa0 * xx + aa1)
+        return F
+    
+    plt.scatter(logEps, logSumWij, s=1, c='C0', edgecolor='#1f77b4', zorder=.1, label='data')
+    plt.plot(logEps, fun(logEps, popt[0], popt[1], popt[2], popt[3]), c='C1', linewidth=.5, zorder=.2, label='tanh(x)')
+    plt.plot(logEps, popt[0]*popt[2]*(logEps+popt[1]/popt[0]) + popt[3], c='C2', linewidth=.5, zorder=.3, label='slope')
+    plt.axvline(x=-(popt[1] / popt[0]), c='C3', linewidth=.5, label='epsilon')
+    
+    plt.legend(loc='best')
+    plt.xlabel(r'$\mathrm{ln \ \epsilon}$', fontsize=16)
+    plt.ylabel(r'$\mathrm{ln \ \sum_{i,j} \ A_{i,j}}$', fontsize=18, rotation=90)
+    plt.ylim(np.amin(fun(logEps, popt[0], popt[1], popt[2], popt[3]))-1, np.amax(fun(logEps, popt[0], popt[1], popt[2], popt[3]))+1)
+
+    slope = popt[0]*popt[2] #slope of tanh
+    eps = -(popt[1] / popt[0]) #x-axis line through center of tanh
+    print('Coefficient of Determination: %s' % R2)
+    print('Slope: %s' % slope) 
+    print('ln(epsilon): %s; epsilon: %s' % (eps, np.exp(eps)))
+    if 0: #plot Gaussian Bandwidth
+        plt.show()
+    if 0: #save Gaussian Bandwidth plot to file
+        np.save('GaussianBandwidth_PD%s.npy' % PD, [logEps, logSumWij])
     
     # =========================================================================
     # Generate optimal Gaussian kernel for Similarity Matrix (A)
@@ -169,7 +182,7 @@ def op(pyDir, PD):
         plt.axhline(y=0, color='k', alpha=.5, linestyle='--', linewidth=1)
         plt.show()
     
-    if 0: #2d diffusion map; sets of higher-order eigenfunction combinations     
+    if 1: #2d diffusion map; sets of higher-order eigenfunction combinations     
         enum = np.arange(1,m+1)
         s = 20
         lw = .5
@@ -211,3 +224,4 @@ if __name__ == '__main__':
     path1 = os.path.splitext(sys.argv[0])[0]
     path2, tail = os.path.split(path1)
     op(path2, sys.argv[1])
+        
